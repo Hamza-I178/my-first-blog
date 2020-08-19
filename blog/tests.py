e0dd90bd472152a12d55f5e9eb526c406b4dc95b
hamza <hamzairfan178@gmail.com>
2020-08-19 14:00:10 +0100

    added a deliberately failing test for my cv template

--- blog/tests.py
+++ blog/tests.py
@@ -5,3 +5,10 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'blog/base.html')
+
+class MyCVTest(TestCase):
+
+    def test_my_cv_page_uses_template(self):
+        response = self.client.get('/my_cv/')
+        self.assertTemplateUsed(response, 'blog/my_cv.html')
+    
