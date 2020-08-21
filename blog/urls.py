05b16f80d1d08174853ee108be9188003467d429
hamza <hamzairfan178@gmail.com>
2020-08-21 12:37:10 +0100

    added a home page, renamed blog to site in page header

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('', views.home, name='home'),
+    path('blog/', views.post_list, name='post_list'),
+    path('blog/post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('blog/post/new/', views.post_new, name='post_new'),
+    path('blog/post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('my_cv/', views.my_cv, name = 'my_cv'),
     path('my_cv_edit/', views.my_cv_edit, name = 'my_cv_edit'),
 ]
