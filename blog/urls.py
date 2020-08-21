31d999ed50841779128a11cdedc4965a83f6a5b1
hamza <hamzairfan178@gmail.com>
2020-08-21 12:52:20 +0100

    minor change to my cv edit url

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('blog/post/new/', views.post_new, name='post_new'),
     path('blog/post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('my_cv/', views.my_cv, name = 'my_cv'),
-    path('my_cv_edit/', views.my_cv_edit, name = 'my_cv_edit'),
+    path('my_cv/edit/', views.my_cv_edit, name = 'my_cv_edit'),
 ]
